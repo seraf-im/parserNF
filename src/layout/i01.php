6a3e6eb50d2e72b55a81e925c1286a7fc2b4551a
Nícola Serafim <nicola@seraf.im>
2023-05-13 14:37:49 -0300

    fix: Grupo I01

--- src/layout/i01.php
+++ src/layout/i01.php
@@ -54,29 +54,36 @@ class i01 extends layout
 
     self::tag($parser, 'No máximo 100 declarações', 'I18', 0, 100);
 
-    $std->nDI               = self::tag((string) $parser->nDI, 'nDI não informado', 'I19', 1);
-    $std->dDI               = self::tag((string) $parser->dDI, 'dDI não informado', 'I20', 1);
-    $std->xLocDesemb        = self::tag((string) $parser->xLocDesemb, 'xLocDesemb não informado', 'I21', 1);
-    $std->UFDesemb          = self::tag((string) $parser->UFDesemb, 'UFDesemb não informado', 'I22', 1);
-    $std->dDesemb           = self::tag((string) $parser->dDesemb, 'dDesemb não informado', 'I23', 1);
-    $std->tpViaTransp       = TpViaTransp::from((int)self::tag((string) $parser->tpViaTransp, 'tpViaTransp não informado', 'I23a', 1));
-    $std->vAFRMM            = self::tag((string) $parser->vAFRMM, 'vAFRMM não informado', 'I23b', 0);
-    $std->tpIntermedio      = TpIntermedio::from((int) self::tag((string) $parser->tpIntermedio, 'tpIntermedio não informado', 'I23c', 1));
-    $std->CNPJ              = self::tag((string) $parser->CNPJ, 'CNPJ não informado', 'I23d', 0);
-    $std->UFTerceiro        = self::tag((string) $parser->UFTerceiro, 'UFTerceiro não informado', 'I23e', 0);
-    $std->cProdcExportador  = self::tag((string) $parser->cExportador, 'cExportador não informado', 'I24', 1);
-
     $i = 0;
+    foreach ($parser as $item) {
+      $r['nDI']               = self::tag((string) $item->nDI, 'nDI não informado', 'I19', 1);
+      $r['dDI']               = self::tag((string) $item->dDI, 'dDI não informado', 'I20', 1);
+      $r['xLocDesemb']        = self::tag((string) $item->xLocDesemb, 'xLocDesemb não informado', 'I21', 1);
+      $r['UFDesemb']          = self::tag((string) $item->UFDesemb, 'UFDesemb não informado', 'I22', 1);
+      $r['dDesemb']           = self::tag((string) $item->dDesemb, 'dDesemb não informado', 'I23', 1);
+      $r['tpViaTransp']       = TpViaTransp::from((int)self::tag((string) $item->tpViaTransp, 'tpViaTransp não informado', 'I23a', 1));
+      $r['vAFRMM']            = self::tag((string) $item->vAFRMM, 'vAFRMM não informado', 'I23b', 0);
+      $r['tpIntermedio']      = TpIntermedio::from((int) self::tag((string) $item->tpIntermedio, 'tpIntermedio não informado', 'I23c', 1));
+      $r['CNPJ']              = self::tag((string) $item->CNPJ, 'CNPJ não informado', 'I23d', 0);
+      $r['UFTerceiro']        = self::tag((string) $item->UFTerceiro, 'UFTerceiro não informado', 'I23e', 0);
+      $r['cProdcExportador']  = self::tag((string) $item->cExportador, 'cExportador não informado', 'I24', 1);
 
-    self::tag($parser->adi, 'No máximo 100 adições', 'I25', 0, 100);
+      $ii = 0;
 
-    if ($parser->adi) {
-      foreach ($parser->adi as $item) {
-        $return[$i] = self::adi($item, $std);
-        $i++;
+      self::tag($item->adi, 'No máximo 100 adições', 'I25', 0, 100);
+
+      if ($item->adi) {
+        unset($return);
+        foreach ($item->adi as $item2) {
+          $return[$ii] = self::adi($item2, $std);
+          $ii++;
+        }
+
+        $r['adi'] = $return;
       }
 
-      $std->adi = $return;
+      $std->$i = $r;
+      $i++;
     }
 
     return $std;
@@ -84,6 +91,7 @@ class i01 extends layout
 
   private static function adi(object $parser): object
   {
+    var_dump($parser);
     $std = new stdClass;
 
     $std->nAdicao           = self::tag((string) $parser->nAdicao, 'nAdicao não informado', 'I26', 1);
