5bee96018ae86b24715fde286f0594135f1e3750
Nícola Serafim <nicola@seraf.im>
2023-05-15 12:34:37 -0300

    refactor: Grupo T

--- examples/t.php
+++ examples/t.php
@@ -33,27 +33,35 @@ declare(strict_types=1);
 ##                                          INICIO CÓDIGO DE FONTE!                                          ##
 ###############################################################################################################
 
-namespace Pnhs\ParserNF\layout;
+ini_set("display_errors", "On");
+ini_set("display_startup_erros", "On");
+error_reporting(E_ALL);
+ini_set("max_execution_time", 5);
 
-use stdClass;
+use Pnhs\ParserNF\layout\h;
+use Pnhs\ParserNF\layout\t;
+use Pnhs\ParserNF\Parser;
 
-class t extends layout
-{
-  public static function run(object $data, int $numero_item, $std = new stdClass): stdClass
-  {
-    $parser = $data->NFe->infNFe->det[$numero_item - 1]->imposto->COFINSST;
+require "../autoload.php";
 
-    if (!$parser)
-      return $std;
+try {
+  if (!file_exists("nota_fiscal.xml")) die("Arquivo nota_fiscal.xml não existe");
+  $xml = file_get_contents("nota_fiscal.xml");
 
-    $std->vBC             = self::tag((string) $parser->vBC, 'vBC não informado', 'T02', 1);
-    $std->pCOFINS         = self::tag((string) $parser->pCOFINS, 'pCOFINS não informado', 'T03', 1);
-    if ($parser->qBCProd || $parser->vAliqProd || $parser->vCOFINS) {
-      $std->qBCProd       = self::tag((string) $parser->qBCProd, 'qBCProd não informado', 'T04', 1);
-      $std->vAliqProd     = self::tag((string) $parser->vAliqProd, 'vAliqProd não informado', 'T05', 1);
-      $std->vCOFINS       = self::tag((string) $parser->vCOFINS, 'vCOFINS não informado', 'T06', 1);
-    }
+  $parser = new parser($xml);
+  $p = $parser->read();
 
-    return $std;
+  $group_h = h::run($p);
+
+  if (is_null($group_h)) die('Nota Fiscal Inválida');
+
+  foreach ($group_h as $item) {
+    $group_t[] = (t::run($p, $item->nItem, $item));
   }
+
+  echo '<p class="display-5">Grupo T</p><p class="h5">COFINS ST</p><pre>';
+  print_r($group_t);
+  echo '</pre>';
+} catch (Exception $e) {
+  die("<b>Erro:</b> " . $e->getMessage());
 }
