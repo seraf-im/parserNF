beb0dac6078d338a81bcc377cd9fda0506f25a7b
Nícola Serafim <nicola@seraf.im>
2023-06-17 16:41:58 -0300

    style: Grupo BA

--- src/layout/ba.php
+++ src/layout/ba.php
@@ -75,30 +75,30 @@ class ba extends layout
 
   private static function refNF(object $parser, stdClass $std): void
   {
-    $std->cUF     = Uf::from((int) self::tag((string)$parser->cUF, 'cUF não informado', 'BA04', 1));
-    $std->AAMM    = self::tag((string) $parser->AAMM, 'AAMM não informado', 'BA05', 1);
-    $std->CNPJ    = self::tag((string) $parser->CNPJ, 'CNPJ não informado', 'BA06', 1);
-    $std->mod     = Mod::from((string) self::tag((string) $parser->mod, 'mod não informado', 'BA07', 1));
-    $std->serie   = self::tag((string) $parser->serie, 'serie não informado', 'BA08', 1);
-    $std->nNF     = self::tag((string) $parser->nNF, 'nNF não informado', 'BA09', 1);
+    $std->cUF           = Uf::from((int) self::tag((string)$parser->cUF, 'cUF não informado', 'BA04', 1));
+    $std->AAMM          = self::tag((string) $parser->AAMM, 'AAMM não informado', 'BA05', 1);
+    $std->CNPJ          = self::tag((string) $parser->CNPJ, 'CNPJ não informado', 'BA06', 1);
+    $std->mod           = Mod::from((string) self::tag((string) $parser->mod, 'mod não informado', 'BA07', 1));
+    $std->serie         = self::tag((string) $parser->serie, 'serie não informado', 'BA08', 1);
+    $std->nNF           = self::tag((string) $parser->nNF, 'nNF não informado', 'BA09', 1);
   }
 
   private static function refNFP(object $parser, stdClass $std): void
   {
-    $std->cUF     = Uf::from((int) self::tag((string)$parser->cUF, 'cUF não informado', 'BA11', 1));
-    $std->AAMM    = self::tag((string) $parser->AAMM, 'AAMM não informado', 'BA12', 1);
-    $std->CNPJ    = self::tag((string) $parser->CNPJ, 'CNPJ não informado', 'BA13', 1);
-    $std->CPF     = self::tag((string) $parser->CPF, 'CPF não informado', 'BA14', 1);
-    $std->IE      = self::tag((string) $parser->IE, 'IE não informado', 'BA15', 1);
-    $std->mod     = Mod::from((string) self::tag((string) $parser->mod, 'mod não informado', 'BA16', 1));
-    $std->serie   = self::tag((string) $parser->serie, 'serie não informado', 'BA17', 1);
-    $std->nNF     = self::tag((string) $parser->nNF, 'nNF não informado', 'BA18', 1);
+    $std->refNFP_cUF    = Uf::from((int) self::tag((string)$parser->cUF, 'cUF não informado', 'BA11', 1));
+    $std->refNFP_AAMM   = self::tag((string) $parser->AAMM, 'AAMM não informado', 'BA12', 1);
+    $std->refNFP_CNPJ   = self::tag((string) $parser->CNPJ, 'CNPJ não informado', 'BA13', 1);
+    $std->refNFP_CPF    = self::tag((string) $parser->CPF, 'CPF não informado', 'BA14', 1);
+    $std->refNFP_IE     = self::tag((string) $parser->IE, 'IE não informado', 'BA15', 1);
+    $std->refNFP_mod    = Mod::from((string) self::tag((string) $parser->mod, 'mod não informado', 'BA16', 1));
+    $std->refNFP_serie  = self::tag((string) $parser->serie, 'serie não informado', 'BA17', 1);
+    $std->refNFP_nNF    = self::tag((string) $parser->nNF, 'nNF não informado', 'BA18', 1);
   }
 
   private static function refECF(object $parser, stdClass $std): void
   {
-    $std->mod     = Mod::from((string) self::tag((string) $parser->mod, 'mod não informado', 'BA21', 1));
-    $std->nECF    = self::tag((string) $parser->nECF, 'nECF não informado', 'BA22', 1);
-    $std->nCOO    = self::tag((string) $parser->nCOO, 'nCOO não informado', 'BA23', 1);
+    $std->refECF_mod   = Mod::from((string) self::tag((string) $parser->mod, 'mod não informado', 'BA21', 1));
+    $std->refECF_nECF  = self::tag((string) $parser->nECF, 'nECF não informado', 'BA22', 1);
+    $std->refECF_nCOO  = self::tag((string) $parser->nCOO, 'nCOO não informado', 'BA23', 1);
   }
 }
